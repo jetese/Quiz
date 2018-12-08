270b250f4931a303d00847f27cbc49ee0ad9d131
jetese <jetese2@gmail.com>
2018-12-08 09:40:30 +0100

    Jueguito

---
+++ Juego/Assets/AcelerometerInput.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AcelerometerInput : MonoBehaviour
+{
+    public Transform rotation;
+    void Update()
+    {
+       transform.Rotate(Input.acceleration.x * 0.1f, 0, 0);
+    }
+}
